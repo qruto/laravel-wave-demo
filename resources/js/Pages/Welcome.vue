61a787aa3de9e9ba6c81c194be35af1361be4076
Slava Razum <razum@qruto.to>
2022-09-09 00:12:31 +0300

    Updated head title

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -28,7 +28,7 @@ setInterval(() => {
 </script>
 
 <template>
-    <Head title="Welcome" />
+    <Head title="Laravel Wave Stream" />
 
     <div
         class="relative flex min-h-screen flex-col items-center justify-center bg-gray-100 px-4 pb-16 pt-12 dark:bg-gray-900"
